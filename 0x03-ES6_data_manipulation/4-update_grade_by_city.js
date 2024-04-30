974315b57acc3c2d80a598b86eb3e52db2028636
OkumuP <okumupita@gmail.com>
2024-04-30 11:12:59 -0700

    Update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,25 +1,12 @@
-export default function updateStudentGradeByCity(
-  students,
-  city,
-  newGrades,
-) {
-  const filterStudents = students.filter(
-    (student) => student.location === city,
-  );
-
-  const gradeStudents = filterStudents.map(
-    (student) => {
-      for (const gradeInfo of newGrades) {
-        if (student.id === gradeInfo.studentId) {
-          student.grade = gradeInfo.grade; 
-        }
-      }
-      if (student.grade === undefined) {
-        student.grade = 'N/A'; 
-      }
-      return student;
-    },
-  );
-
-  return gradeStudents;
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  return arr
+    .filter((el) => el.location === city)
+    .map((person) => {
+      const grades = newGrades.filter((el) => el.studentId === person.id);
+      const grade = grades.length ? grades[0].grade : 'N/A';
+      return {
+        ...person,
+        grade,
+      };
+    });
 }
